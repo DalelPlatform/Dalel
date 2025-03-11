1970a5207c1e783981c95ac11b2eaf8d3a1f067f
ReemAshraf18 <94737100+ReemAshraf18@users.noreply.github.com>
2025-03-11 20:44:33 +0200

    Preview Reem Task

---
+++ Models/HomeService/CategoryServices.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.HomeService
+{
+    public class CategoryServices
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public virtual ICollection<ServiceProvider> ServiceProviders { get; set; } = new List<ServiceProvider>();
+        public virtual ICollection<ServiceQuaries> Quaries { get; set; } = new List<ServiceQuaries>();
+    }
+
+    public class CategoryServicesConfiguration : IEntityTypeConfiguration<CategoryServices>
+    {
+        public void Configure(EntityTypeBuilder<CategoryServices> builder)
+        {
+            builder.HasKey(cs => cs.Id);
+
+            builder.Property(cs => cs.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(cs => cs.Image)
+                .HasMaxLength(255);
+
+            builder.Property(cs => cs.Description)
+                .HasMaxLength(500);
+
+            builder.HasMany(cs => cs.ServiceProviders)
+                .WithOne(sp => sp.CategoryServices)
+                .HasForeignKey(sp => sp.CategoryServicesId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(cs => cs.Quaries)
+                .WithOne(sq => sq.CategoryServices)
+                .HasForeignKey(sq => sq.CategoryServicesId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasData(
+                new CategoryServices { Id = 1, Name = "Plumbing", Description = "Plumbing services" },
+                new CategoryServices { Id = 2, Name = "Electrical", Description = "Electrical services" }
+            );
+        }
+    }
+}
---
+++ Models/HomeService/ServiceQuaries.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.HomeService
+{
+    public class ServiceQuaries
+    {
+        public int Id { get; set; }
+        public int ServiceProviderId { get; set; }
+        public int ClientId { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public DateTime QuestionDate { get; set; }
+        public DateTime AnswerDate { get; set; }
+        public int CategoryServicesId { get; set; }
+        public virtual CategoryServices CategoryServices { get; set; }
+        public virtual ServiceProvider ServiceProvider { get; set; }
+    }
+    public class ServiceQuariesConfiguration : IEntityTypeConfiguration<ServiceQuaries>
+    {
+        public void Configure(EntityTypeBuilder<ServiceQuaries> builder)
+        {
+            builder.HasKey(sq => sq.Id);
+            builder.Property(sq => sq.Question)
+                .HasMaxLength(500);
+            builder.Property(sq => sq.Answer)
+                .HasMaxLength(500);
+            builder.HasOne(sq => sq.CategoryServices)
+                .WithMany(cs => cs.Quaries)
+                .HasForeignKey(sq => sq.CategoryServicesId)
+                .OnDelete(DeleteBehavior.Cascade);
+            builder.HasOne(sq => sq.ServiceProvider)
+                .WithMany(sp => sp.)
+                .HasForeignKey(sq => sq.ServiceProviderId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
