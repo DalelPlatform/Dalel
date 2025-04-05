a5d1ecc0331ff1ee34139e20ba0458d8633ce3c1
ReemAshraf18 <94737100+ReemAshraf18@users.noreply.github.com>
2025-04-05 19:48:28 +0200

    API LYER

--- Dalel.API/Program.cs
+++ Dalel.API/Program.cs
@@ -8,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ServiceProviderProjectRepository>();
+builder.Services.AddScoped<ServiceProviderPropsalRepository>();
+builder.Services.AddScoped<ServiceProviderScheduleRepository>();
 
 var app = builder.Build();
 
@@ -20,9 +23,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseAuthorization();
 // Add this to your Program.cs
-builder.Services.AddScoped<ServiceProviderProjectRepository>();
-builder.Services.AddScoped<ServiceProviderPropsalRepository>();
-builder.Services.AddScoped<ServiceProviderScheduleRepository>();
+
 app.MapControllers();
 
 app.Run();
