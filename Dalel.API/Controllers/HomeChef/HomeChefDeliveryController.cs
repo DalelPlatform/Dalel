4aaf305a0ab0a0b6fdadf6ad185193ddabdcd16c
Osamatalaat <osamatalaat707@gmail.com>
2025-04-19 01:28:25 +0200

    pulling ...

--- Dalel.API/Controllers/HomeChef/HomeChefDeliveryController.cs
+++ Dalel.API/Controllers/HomeChef/HomeChefDeliveryController.cs
@@ -44,16 +44,16 @@ namespace Dalel.API.Controllers.HomeChef
         }
 
         [Authorize(Roles = "Client,Admin,HomeChef")]
-        [HttpPost("UpdateOrder")]
+        [HttpPost("UpdateOrder/{id}")]
 
-        public IActionResult UpdateDeliveryOrder(AddHomeChefDeliveryVM deliveryVm)
+        public IActionResult UpdateDeliveryOrder(int id ,AddHomeChefDeliveryVM deliveryVm)
         {
             if (!ModelState.IsValid)
             {
                 return new JsonResult("Invalid data provided");
             }
 
-            var result = _homeChefService.UpdateDeliveryOrder(deliveryVm);
+            var result = _homeChefService.UpdateDeliveryOrder(id ,deliveryVm);
             if (result.Success)
             {
                 return new JsonResult(result);
