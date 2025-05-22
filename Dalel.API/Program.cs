cb6d6190692be85a527b1bf26ed3859bc9212fcc
hebamsamy <hebasamy655@gmail.com>
2025-05-22 21:33:27 +0300

    fix program.cs

--- Dalel.API/Program.cs
+++ Dalel.API/Program.cs
@@ -28,10 +28,6 @@ using Utilities.Payments.Gateways;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddDbContext<DelelContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DalelDB")));
 // Register AutoMapper
 
 builder.Services.AddControllers().AddJsonOptions(options =>
@@ -41,7 +37,6 @@ builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull; // Ignore null values
 });
 
-builder.Services.AddEndpointsApiExplorer();
 //To Enable Swagger to test authentication token >>(Bearer space token)
 builder.Services.AddSwaggerGen(c => {
     c.SwaggerDoc("v1", new OpenApiInfo
@@ -74,12 +69,6 @@ builder.Services.AddDbContext<DelelContext>
     (i => i.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("DalelDB")));
 builder.Services.AddIdentity<AppUser, IdentityRole>()
     .AddEntityFrameworkStores<DelelContext>();
-
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 
 //Add srilog logging
 Log.Logger = new LoggerConfiguration()  
@@ -198,12 +187,7 @@ builder.Services.AddScoped<IPaymentProcessor<PaymentVehicle>, DriverPaymentProce
 
 #endregion
 
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-        options.JsonSerializerOptions.MaxDepth = 64;
-    });
+
 
 builder.Services.AddAuthentication(option =>
 {
@@ -245,8 +229,6 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
-app.UseCors();
-// Add this to your Program.cs
 
 app.MapControllerRoute(
     name: "default",
@@ -255,7 +237,5 @@ app.MapControllerRoute(
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=admin}/{controller=Home}/{action=Index}");
-
-app.MapControllers();
 
 app.Run();
